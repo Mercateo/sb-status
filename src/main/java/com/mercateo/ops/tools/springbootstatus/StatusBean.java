ca4e0c51b3509fc5f304f0ecc1d307485001f208
Michael Weirauch <michael.weirauch@gmail.com>
2016-06-27 13:02:51 +0200

    health endpoint does not return json
    * contains an optimistic approch parsing every "status" contained within
      the queried health data
    * "UNKNOWN" is treated as "UP", as that is what I observed here from
      the root aggregate status

--- src/main/java/com/mercateo/ops/tools/springbootstatus/StatusBean.java
+++ src/main/java/com/mercateo/ops/tools/springbootstatus/StatusBean.java
@@ -1,14 +1,53 @@
 package com.mercateo.ops.tools.springbootstatus;
 
 class StatusBean {
-    @Override
-    public String toString() {
-        return "StatusBean [status=" + this.status + "]";
-    }
 
-    String status = "unknown";
+    private static final String KEY = "status=";
+
+    private static final String ACTUATOR_STATUS_UP = "UP";
+
+    private static final String ACTUATOR_STATUS_UNKNOWN_ABBR = "UN";
+
+    private final String status;
+
+    StatusBean(String status) {
+        this.status = status;
+    }
 
     public boolean isUp() {
-        return this.status.equalsIgnoreCase("up");
+        if (null == status || status.trim().length() == 0) {
+            return false;
+        }
+
+        int idx = status.indexOf(KEY);
+        if (idx < 0) {
+            return false;
+        }
+        boolean up = true;
+        while (idx >= 0) {
+            int idxValStart = idx + KEY.length();
+            int idxValEnd = idxValStart + 2;
+
+            String val = status.substring(idxValStart, idxValEnd);
+
+            /*
+             * Actuator reports "UP" even if one or more are "UNKNOWN". (At
+             * least with 1.3.5.RELEASE)
+             */
+            if (!ACTUATOR_STATUS_UP.equals(val) && !ACTUATOR_STATUS_UNKNOWN_ABBR.equals(val)) {
+                up = false;
+                break;
+            }
+
+            idx = status.indexOf(KEY, idxValEnd);
+        }
+
+        return up;
     }
+
+    @Override
+    public String toString() {
+        return "StatusBean [status=" + (isUp() ? ACTUATOR_STATUS_UP : "UNKNOWN") + "]";
+    }
+
 }
